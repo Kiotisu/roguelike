447c0b3548e898fda494b5eabfadb6d2451446a4
Kiotisu <f7x@interia.pl>
2015-06-08 01:06:39 +0200

    fix

--- roguelike/src/pygs.py
+++ roguelike/src/pygs.py
@@ -462,7 +462,7 @@ class App(object):
 
             elif action_pos[2] == "rf":
                 self._hero.restore_hp(95)
-                self._hero.add_hp()
+                self._hero.gain_exp(100)
                 action_pos[2] = "bf"
             else:
                 print "gracz atakuje"
